2c8fecc16c6afdd2a335b287a4cf59472e0fc868
Nicolaspz20 <nicolasperez1114@gmail.com>
2024-03-01 16:24:20 -0300

    añadiendo botones interactivosç

--- primerproyecto/vite.config.js
+++ primerproyecto/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import '/BotonAgregar.css';
+import { useState } from 'react';
+const BotonAgregar = () => { 
+const [constador, setcontador] =
+useState (0);
+ const handleAgregar = () => {
+    setcontador(contador + 1);
+};
+return(
+<div>
+<button className='boton-agregar' onClick={handleAgregar}>agregar</button>
+<p>contador: {contador}</p>
+</div>
+);
+};
+
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
